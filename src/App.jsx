282ced502ed747d414e6071dab22e73f7c927089
FabioJunik <fabiojunik@gmail.com>
2022-08-24 15:35:54 +0100

    Definindo os estilos globais

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Header } from './components/Header'
+import './global.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <Header/>
+    </div>
+  )
+}
+
+export default App
