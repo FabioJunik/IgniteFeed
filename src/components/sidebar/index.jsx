feeac02a942f04661f92bda8c9409f00860f6555
FabioJunik <fabiojunik@gmail.com>
2022-08-26 19:06:10 +0100

    Finalizando o SidebarD

--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -1,3 +1,4 @@
+import {PencilLine} from 'phosphor-react';
 import styles from './styles.module.css';
 
 export function Sidebar(){
@@ -8,12 +9,16 @@ export function Sidebar(){
                 src="https://images.unsplash.com/photo-1624996752380-8ec242e0f85d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDR8fGRldmVsb3BlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=300&q=50"
             />
             <div className={styles.profile}>
+                <img src="https://github.com/fabiojunik.png"/>
                 <strong>Fábio Junik</strong>
                 <span>Front End Developer</span>
             </div>
 
             <footer>
-                <a href="#">Editar seu perfil</a>
+                <a href="#">
+                    <PencilLine size={20}/>
+                    Editar seu perfil
+                </a>
             </footer>
         </aside>
     )
