48acfe858f42437e119ac8e08bfb0522e4a70dfd
FabioJunik <fabiojunik@gmail.com>
2022-08-28 07:34:16 +0100

    Estilização do Post

--- src/components/post/index.jsx
+++ src/components/post/index.jsx
@@ -9,20 +9,24 @@ export function Post(){
                     <img className={styles.avatar} src='https://github.com/fabiojunik.png'/>
                     <div className={styles.authorInfo}>
                         <strong>Fábio Junik</strong>
-                        <span>Wev Developer</span>
+                        <span>Web Developer</span>
                     </div>
                 </div>
                 <time title='08 de Agosto de 2022 as 06:40' dateTime='2022-08-28 06:40'>Públicado há 1 hora</time>
-                <div className={styles.content}>
+            </header>
+            <div className={styles.content}>
                     <p>Fala galeraa 👋</p>
 
                     <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
 
                     <p><a href='#'>👉 jane.design/doctorcare</a></p>
 
-                    <p><a href='#'>#novoprojeto #nlw #rocketseat</a></p>
+                    <p>
+                        <a href='#'>#novoprojeto</a>{' '}
+                        <a href='#'>#nlw</a>{' '}
+                        <a href='#'>#rocketseat</a>
+                    </p>
                 </div>
-            </header>
         </article>
     )
 }
